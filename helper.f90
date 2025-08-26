9bf0a86671b77ebb2825cac052cdad8e250cbc1c
jkgoates <jeremiahgoates@gmail.com>
2025-08-26 14:47:39 -0600

    Starting standard atmosphere

--- helper.f90
+++ helper.f90
@@ -1,11 +1,18 @@
 module helper_m
+
     implicit none
+
+    ! CONSTANTS
     real,parameter :: PI = 3.141592653589793
     real,parameter :: PI2 = PI*0.5
     real,parameter :: PI4 = PI*0.25
+    real,parameter :: g_ssl = 9.80665 ! Standard Gravity at sea level 
+    real,parameter :: r_ez = 6356766.0 ! US standard atmosphere earth radius in meters
     
 contains
     
+    !!! COORDINATE TRANSFORM !!!
+
     function quat_mult(q1, q2) result(q3)
 
         implicit none
@@ -13,17 +20,10 @@ contains
         real, intent(in) :: q1(4), q2(4)
         real :: q3(4)
 
-        ! 1.31250000 secs
         q3(1) = q1(1)*q2(1) - q1(2)*q2(2) - q1(3)*q2(3) - q1(4)*q2(4)
         q3(2) = q1(1)*q2(2) + q1(2)*q2(1) + q1(3)*q2(4) - q1(4)*q2(3)
         q3(3) = q1(1)*q2(3) - q1(2)*q2(4) + q1(3)*q2(1) + q1(4)*q2(2)
         q3(4) = q1(1)*q2(4) + q1(2)*q2(3) - q1(3)*q2(2) + q1(4)*q2(1)
-
-        ! 1.65625000 secs
-        !q3 = (/q1(1)*q2(1) - q1(2)*q2(2) - q1(3)*q2(3) - q1(4)*q2(4),&
-                !q1(1)*q2(2) + q1(2)*q2(1) + q1(3)*q2(4) - q1(4)*q2(3),&
-                !q1(1)*q2(3) - q1(2)*q2(4) + q1(3)*q2(1) + q1(4)*q2(2), &
-                !q1(1)*q2(4) + q1(2)*q2(3) - q1(3)*q2(2) + q1(4)*q2(1)/)
 
     end function quat_mult
 
@@ -35,7 +35,6 @@ contains
         real, intent(in) :: q1(4), vec1(3)
         real :: vec2(3)
 
-        ! explicit cross mult approach FASTEST
         real :: t(4)
 
         t(1) = -vec1(1)*q1(2) - vec1(2)*q1(3) - vec1(3)*q1(4)
@@ -83,6 +82,8 @@ contains
 
 
     function euler_to_quat(e1) result(q1)
+        implicit none
+        
         real, intent(in) :: e1(3)
         real :: q1(4)
 
@@ -110,6 +111,8 @@ contains
 
 
     function quat_to_euler(q1) result(e1)
+        implicit none
+        
         real, intent(in) :: q1(4)
         real :: e1(3)
 
@@ -136,9 +139,84 @@ contains
         
     end function quat_to_euler
 
+    !!! STD ATMOSPHERE !!!
+
+    function gravity_SI(h) result(g)
+        implicit none
+        
+        real, intent(in) :: h 
+        real :: g
+
+        g = g_ssl*(r_ez/(r_ez + h))**2
+        
+    end function gravity_SI
+
+    function gravity_English(h_ft) result(g)
+        implicit none
+        
+        real, intent(in) :: h_ft
+        real :: g
+
+        g = meters_to_feet(gravity_SI(feet_to_meters(h_ft)))
+        
+    end function gravity_English
 
 
+    !!! UNIT CONVERSIONS !!!
+
+    function feet_to_meters(l_ft) result(l_m)
+
+        implicit none
+        
+        real, intent(in) :: l_ft
+        real :: l_m
+
+        l_m = 0.3048*l_ft
+
+    end function feet_to_meters
+
+    function meters_to_feet(l_m) result(l_ft)
+
+        implicit none
+        
+        real, intent(in) :: l_m
+        real :: l_ft
+
+        l_ft = 3.280839895013123*l_m
+
+
+    end function meters_to_feet
 
 
 
 end module helper_m
+
+
+!program test_gravity
+
+    !use helper_m
+
+    !implicit none
+
+    !integer :: i, j
+    !real :: g
+
+    !write(*,*) "SI units"
+    !do i = 0, 100000, 5000
+        !g = gravity_SI(real(i))
+
+        !write(*,*) "Altitude:", i, " meters. Gravity", g, "m/s2"
+
+    !end do
+
+    !write(*,*)
+    !write(*,*) "ENGLISH UNITS"
+
+    !do j = 0, 200000, 10000
+        !g = gravity_English(real(j))
+
+        !write(*,*) "Altitude:", j, " meters. Gravity", g, "ft/s2"
+
+    !end do
+    
+!end program test_gravity
